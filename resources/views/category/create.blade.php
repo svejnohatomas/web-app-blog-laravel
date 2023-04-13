1ffcc93358ff805db7f3506073c75cea9e32ea25
Tomas Svejnoha <svejnohatomas@users.noreply.github.com>
2023-04-14 00:12:42 +0100

    Add TODO to view

--- resources/views/category/create.blade.php
+++ resources/views/category/create.blade.php
@@ -1,4 +1,16 @@
-<p>Create Category</p>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Create Category') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-6">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <!-- TODO: Add form here -->
+        </div>
+    </div>
+</x-app-layout>
 
 <!--
     TODO: Add @csrf
