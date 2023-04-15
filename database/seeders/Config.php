63a2dc4764c754617bcef74fbe9280f7ed0d39d5
Tomas Svejnoha <svejnohatomas@users.noreply.github.com>
2023-04-15 22:57:37 +0200

    Change seeding config

--- database/seeders/Config.php
+++ database/seeders/Config.php
@@ -4,8 +4,8 @@ namespace Database\Seeders;
 
 class Config
 {
-    static int $numberOfCategories = 5 * 6;
-    static int $numberOfPosts = 50 * 6;
-    static int $numberOfUsers = 10 * 6;
-    static int $numberOfComments = 100 * 6;
+    static int $numberOfCategories = 20;
+    static int $numberOfPosts = 50 * 20;
+    static int $numberOfUsers = 10;
+    static int $numberOfComments = 50000;
 }
