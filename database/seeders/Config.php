e67287e5ce40f19ec1e9ef888a76ae7cf6e41580
Tomas Svejnoha <svejnohatomas@users.noreply.github.com>
2023-04-15 22:27:44 +0200

    Increase the number of seeded entities

--- database/seeders/Config.php
+++ database/seeders/Config.php
@@ -4,8 +4,8 @@ namespace Database\Seeders;
 
 class Config
 {
-    static int $numberOfCategories = 5;
-    static int $numberOfPosts = 50;
-    static int $numberOfUsers = 10;
-    static int $numberOfComments = 100;
+    static int $numberOfCategories = 5 * 6;
+    static int $numberOfPosts = 50 * 6;
+    static int $numberOfUsers = 10 * 6;
+    static int $numberOfComments = 100 * 6;
 }
