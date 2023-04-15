6abd9915fb4e74e8ff6f6154fd07d9a80157da9f
Tomas Svejnoha <svejnohatomas@users.noreply.github.com>
2023-04-15 13:27:12 +0100

    Add 'username' to User

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -38,6 +38,7 @@ class RegisteredUserController extends Controller
 
         $user = User::create([
             'name' => $request->name,
+            'username' => str_replace(" ", "-", strtolower($request->name)),
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,6 +23,7 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake(FactoriesConfig::$locale)->name(),
+            'username' => fake(FactoriesConfig::$locale)->unique()->userName(),
             'email' => fake(FactoriesConfig::$locale)->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
