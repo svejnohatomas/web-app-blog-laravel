a0affeca4510f1a97a9f79275cba4c0425480902
Tomas Svejnoha <svejnohatomas@users.noreply.github.com>
2023-04-15 22:26:59 +0200

    Require authenticated user

--- routes/web.php
+++ routes/web.php
@@ -32,59 +32,61 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-// TODO: Add route constraints
-Route::controller(CategoryController::class)->group(function () {
-    // Create
-    Route::get('/categories/create', [CategoryController::class, 'create'])->name('category.create');
-    Route::post('/categories/create', [CategoryController::class, 'store'])->name('category.store');
+Route::group(['middleware' => ['auth']], function () {
+    // TODO: Add route constraints
+    Route::controller(CategoryController::class)->group(function () {
+        // Create
+        Route::get('/categories/create', [CategoryController::class, 'create'])->name('category.create');
+        Route::post('/categories/create', [CategoryController::class, 'store'])->name('category.store');
 
-    // Read
-    Route::get('/categories', [CategoryController::class, 'index'])->name('category.index');
-    Route::get('/categories/{slug}', [CategoryController::class, 'show'])->name('category.show');
+        // Read
+        Route::get('/categories', [CategoryController::class, 'index'])->name('category.index');
+        Route::get('/categories/{slug}', [CategoryController::class, 'show'])->name('category.show');
 
-    // Update
-    Route::get('/categories/edit/{slug}', [CategoryController::class, 'edit'])->name('category.edit');
-    Route::put('/categories/edit/{id}', [CategoryController::class, 'update'])->name('category.update')->whereNumber('id');
+        // Update
+        Route::get('/categories/edit/{slug}', [CategoryController::class, 'edit'])->name('category.edit');
+        Route::put('/categories/edit/{id}', [CategoryController::class, 'update'])->name('category.update')->whereNumber('id');
 
-    // Delete
-    Route::delete('/categories/delete/{id}', [CategoryController::class, 'destroy'])->name('category.destroy')->whereNumber('id');
-});
+        // Delete
+        Route::delete('/categories/delete/{id}', [CategoryController::class, 'destroy'])->name('category.destroy')->whereNumber('id');
+    });
 
-// TODO: Add route constraints
-Route::controller(PostController::class)->group(function () {
-    // Create
-    Route::get('/categories/{categorySlug}/posts/create', [PostController::class, 'create'])->name('post.create');
-    Route::post('/posts/create', [PostController::class, 'store'])->name('post.store');
+    // TODO: Add route constraints
+    Route::controller(PostController::class)->group(function () {
+        // Create
+        Route::get('/categories/{categorySlug}/posts/create', [PostController::class, 'create'])->name('post.create');
+        Route::post('/posts/create', [PostController::class, 'store'])->name('post.store');
 
-    // Read
-    Route::get('/posts/{slug}', [PostController::class, 'show'])->name('post.show');
+        // Read
+        Route::get('/posts/{slug}', [PostController::class, 'show'])->name('post.show');
 
-    // Update
-    Route::get('/posts/edit/{slug}', [PostController::class, 'edit'])->name('post.edit');
-    Route::put('/posts/edit/{id}', [PostController::class, 'update'])->name('post.update')->whereNumber('id');
+        // Update
+        Route::get('/posts/edit/{slug}', [PostController::class, 'edit'])->name('post.edit');
+        Route::put('/posts/edit/{id}', [PostController::class, 'update'])->name('post.update')->whereNumber('id');
 
-    // Delete
-    Route::delete('/posts/delete/{id}', [PostController::class, 'destroy'])->name('post.destroy')->whereNumber('id');
-});
+        // Delete
+        Route::delete('/posts/delete/{id}', [PostController::class, 'destroy'])->name('post.destroy')->whereNumber('id');
+    });
 
-Route::controller(CommentController::class)->group(function () {
-    // Create
-    Route::post('/comments/create', [CommentController::class, 'store'])->name('comment.store');
+    Route::controller(CommentController::class)->group(function () {
+        // Create
+        Route::post('/comments/create', [CommentController::class, 'store'])->name('comment.store');
 
-    // Read - none needed
+        // Read - none needed
 
-    // Update
-    Route::get('/comments/edit/{id}', [CommentController::class, 'edit'])->name('comment.edit')->whereNumber('id');
-    Route::put('/comments/edit/{id}', [CommentController::class, 'update'])->name('comment.update')->whereNumber('id');
+        // Update
+        Route::get('/comments/edit/{id}', [CommentController::class, 'edit'])->name('comment.edit')->whereNumber('id');
+        Route::put('/comments/edit/{id}', [CommentController::class, 'update'])->name('comment.update')->whereNumber('id');
 
-    // Delete
-    Route::delete('/comments/delete/{id}', [CommentController::class, 'destroy'])->name('comment.destroy')->whereNumber('id');
-});
+        // Delete
+        Route::delete('/comments/delete/{id}', [CommentController::class, 'destroy'])->name('comment.destroy')->whereNumber('id');
+    });
 
-// TODO: Add route constraints
-Route::controller(UserController::class)->group(function () {
-    // Read
-    Route::get('/users/{username}', [UserController::class, 'show'])->name('user.show');
+    // TODO: Add route constraints
+    Route::controller(UserController::class)->group(function () {
+        // Read
+        Route::get('/users/{username}', [UserController::class, 'show'])->name('user.show');
+    });
 });
 
 require __DIR__.'/auth.php';
