a118bad58c0697e9daec50a8ad428c4329e18bee
Tomas Svejnoha <svejnohatomas@users.noreply.github.com>
2023-05-05 19:45:39 +0100

    Fix comment UI

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -46,26 +46,28 @@
                         div.classList.add("mt-4");
 
                         div.innerHTML = `
-                            <div class="flex space-x-3">
+                            <div class="flex space-x-3 items-center">
                                 <div class="flex-1">
-                                    <h4>
+                                    <h3>
                                         <a class="underline hover:no-underline" href="{{ URL::to('/') }}/users/${response['user_username']}">
                                             ${response['user_name']}
                                         </a> (${response['created_at'].slice(0, 19).replace('T', ' ')})
-                                    </h4>
+                                    </h3>
                                 </div>
                                 <div class="flex-none text-gray-800 dark:text-gray-200">
-                                    <a href="{{ URL::to('/') }}/comments/edit/${response['id']}">Edit</a>
+                                    <a href="{{ URL::to('/') }}/comments/edit/${response['id']}">
+                                        <button type="button" class="inline-flex items-center px-4 py-2 bg-white dark:bg-gray-800 border border-gray-300 dark:border-gray-500 rounded-md font-semibold text-xs text-gray-700 dark:text-gray-300 uppercase tracking-widest shadow-sm hover:bg-gray-50 dark:hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 disabled:opacity-25 transition ease-in-out duration-150">Edit</button>
+                                    </a>
                                 </div>
                                 <div class="flex-none text-gray-800 dark:text-gray-200">
                                     <form id="commentForm${response['id']}" method="POST" action="{{ URL::to('/') }}/comments/delete/${response['id']}">
                                         <input type="hidden" name="_method" value="DELETE">
                                         <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                                        <button onclick="removeComment('${response['id']}')">Delete</button>
+                                        <button class="inline-flex items-center px-4 py-2 bg-red-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-red-500 active:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150" onclick="removeComment('${response['id']}')">Delete</button>
                                     </form>
                                 </div>
                             </div>
-                            <div>
+                            <div class="mt-3">
                                 <p class="px-2 italic">${response['content']}</p>
                             </div>`;
 
